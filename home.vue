dc0c60809df6d385dc770d5d00cc1f409a12fd35
zhaoyouya <1445089819@qq.com>
2023-08-22 09:23:10 +0800

    添加健康等议题

--- home.vue
+++ home.vue
@@ -1,15 +1,12 @@
 
 <template>
-    <div style="display:flex; justify-content:center;font-size: 20px;padding: 20px;line-height: 35px;
-    
-    ">
+    <div style="display:flex; justify-content:center;font-size: 20px;padding: 20px;line-height: 35px;">
         <div style="max-width: 1000px;
         color: var(--vp-c-text-1);
         ">
             <p>正好有个云服务器,做了一个blog网站。但无奈水平有限，也不是很勤快，写不出好的东西，现在是个人的学习和练习笔记</p>
 
             <p> 我常用的编程语言: c++,c#,js/ts,python,对zig非常感兴趣,不喜欢c#。 准备多学习数据库方面的技术，笔记的内容将体现这些。此外，还会记录一些杂 杂 杂的东西</p>
-
             <p>
                 有啥建议，欢迎和我交流，等我成为大佬，我就把这些很low的东西都改了😤！
             </p>
