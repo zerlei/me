0de0331b8f89911c589880ccffabfafb87cc1dc9
zerlei <1445089819@qq.com>
2024-09-19 13:48:10 +0800

    chore: pre-commit 测试

--- filestimeinfo.mjs
+++ filestimeinfo.mjs
@@ -1,32 +1,28 @@
 #!/usr/bin/env zx
+import fs from 'fs'
+console.log("post-commit 开始执行")
+const gitfiles = await $`git ls-tree -r --name-only HEAD`;
 
-import fs from 'fs';
-const SKIP_POST_COMMIT = await $`echo $SKIP_POST_COMMIT`
-console.log(SKIP_POST_COMMIT.stdout)
-//if (process.env.SKIP_POST_COMMIT == 'true') {
-//  await $`SKIP_POST_COMMIT=false`;
-//} else {
-//  console.log('post-commit 开始执行');
-//  const gitfiles = await $`git ls-tree -r --name-only HEAD`;
-//
-//  await $`SKIP_POST_COMMIT=true`;
-//
-//  const files = gitfiles.stdout.split('\n').filter((item) => {
-//    return item.includes('.md');
-//  });
-//  let ar = [];
-//  for (var file of files) {
-//    const createTimeResult = await $`git log --pretty=format:"%ai" -- ${file} | tail -1`;
-//    const modifyTimeResult = await $`git log -1 --pretty=format:"%ai" -- ${file}`;
-//    let ob = [file, createTimeResult.stdout.split('+')[0], modifyTimeResult.stdout.split('+')[0]];
-//    ar.push(ob);
-//  }
-//  try {
-//    fs.writeFileSync('/home/zerlei/git/me/.vitepress/filesTime.json', JSON.stringify(ar));
-//  } catch (err) {
-//    console.error(err);
-//  }
-//  await $`git add .`;
-//  await $`git commit --amend --no-edit --no-verify`;
-//  console.log('post-commit 结束执行');
-//}
+await  $`SKIP_POST_COMMIT=true`
+
+const files = gitfiles.stdout.split('\n').filter((item) => {
+  return item.includes('.md');
+});
+let ar = [];
+for (var file of files) {
+  const createTimeResult = await $`git log --pretty=format:"%ai" -- ${file} | tail -1`;
+  const modifyTimeResult = await $`git log -1 --pretty=format:"%ai" -- ${file}`;
+  let ob = [file, createTimeResult.stdout.split('+')[0], modifyTimeResult.stdout.split('+')[0]];
+  ar.push(ob);
+}
+try {
+  fs.writeFileSync('/home/zerlei/git/me/.vitepress/filesTime.json',JSON.stringify(ar))
+}
+catch (err) {
+console.error(err)
+}
+await $`git add .`
+await $`mv ./.git/hooks/post-commit ./.git/hooks/post-commit.bak`
+await $`git commit --amend --no-edit --no-verify`
+await $`mv ./.git/hooks/post-commit.bak ./.git/hooks/post-commit`
+console.log("post-commit 结束执行")
