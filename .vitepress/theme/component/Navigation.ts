3cfb0fee26447ca7a7dba1b2c7e3fda87a40659a
zerlei <1445089819@qq.com>
2025-05-07 11:15:02 +0800

    chore: add some navigation

--- .vitepress/theme/component/Navigation.ts
+++ .vitepress/theme/component/Navigation.ts
@@ -1,8 +1,14 @@
 export const NavigationData = [
   {
+    title: '新闻网站',
+    children: [
+      ['人民日报', 'http://www.people.com.cn/favicon.ico', '不用https 又怎样？！对于一个普通的劳动人民，他一定是想让你过的好的', 'http://www.people.com.cn/'],
+      ['tiobeIndex', 'https://www.tiobe.com/wp-content/themes/tiobe/favicon/favicon.ico', '编程语言流行度排名，是存量排名', 'https://www.tiobe.com/tiobe-index/']
+    ]
+  },
+  {
     title: '开发者社区',
     children: [
-      ['tiobeIndex', 'https://www.tiobe.com/wp-content/themes/tiobe/favicon/favicon.ico', '编程语言流行度排名，是存量排名', 'https://www.tiobe.com/tiobe-index/'],
       [
         'Dev',
         'https://res.cloudinary.com/practicaldev/image/fetch/s--E8ak4Hr1--/c_limit,f_auto,fl_progressive,q_auto,w_32/https://dev-to.s3.us-east-2.amazonaws.com/favicon.ico',
@@ -65,10 +71,16 @@ export const NavigationData = [
   },
   {
     title: '前端',
-    children: []
+    children: [
+      ['vue', 'https://vuejs.org/logo.svg', '如果是使用vue，仅官方网站的文档就够用了,vue 全家桶(1/3) vue core', 'https://vuejs.org/'],
+      ['vue-router', 'https://vuejs.org/logo.svg', '前端路由，vue 全家桶(2/3) vue router', 'https://router.vuejs.org/'],
+      ['pinia', 'https://pinia.vuejs.org/logo.svg', '全局状态管理，大型项目更偏向使用，vue 全家桶(3/3) pinia ', 'https://pinia.vuejs.org/'],
+      ['naive-ui', 'https://www.naiveui.com/assets/naivelogo-BdDVTUmz.svg', '很有意思的vue 组件库,优雅，项目质量天花板的高', 'https://www.naiveui.com/'],
+      ['t-Design', 'https://cdc.cdn-go.cn/tdc/latest/images/tdesign.svg', '腾讯出的全家桶组件库，非常适合团队项目使用,功能十分强大', 'https://tdesign.tencent.com/']
+    ]
   },
   {
-    title: '🧑🏻‍💻计算机科学',
+    title: '数据结构、算法和数学',
     children: [['Hello 算法', 'https://www.hello-algo.com/assets/images/favicon.png', '对于基本的数据结构和算法，不需要其它的了，免费，强大！', 'https://www.hello-algo.com/']]
   },
   {
@@ -92,6 +104,18 @@ export const NavigationData = [
         'https://godbolt.org/favicon.ico?v=1',
         '你的代码编译之后的样子?通产是各种中间码和汇编码，你可能更关注c/c++ zig等系统级编程语言',
         'https://godbolt.org/'
+      ],
+      ['词根词缀词典', 'https://www.dicts.cn/favicon.ico', '超级好用的词根词缀词典，作者是个自由开发者', 'https://www.dicts.cn/']
+    ]
+  },
+  {
+    title: '软件工程',
+    children: [
+      [
+        'REFACTORING',
+        'https://refactoring.guru/favicon.png',
+        '在这个网站中可以学习到设计模式和代码重构，质量非常好，钱相对充足的时候去支持一下作者，可以微信支付宝。',
+        'https://refactoring.guru/'
       ]
     ]
   },
@@ -101,18 +125,30 @@ export const NavigationData = [
       ['cloudflare', 'https://dash.cloudflare.com/favicon.ico', '活菩萨！！！ 好用的ssl服务，静态网页托管(github 仓库直连)，数据库，完全免费！', 'https://dash.cloudflare.com/'],
 
       [
-        'REFACTORING',
-        'https://refactoring.guru/favicon.png',
-        '在这个网站中可以学习到设计模式和代码重构，质量非常好，钱相对充足的时候去支持一下作者，可以微信支付宝。',
-        'https://refactoring.guru/'
-      ],
-      [
         'Coursela',
         'https://d3njjcbhbojbot.cloudfront.net/web/images/favicons/favicon-v2-32x32.png',
         '人间充满爱的学习平台,质量很高。能在这里找的学习资源，就在这里找吧，很多免费的资源，付费也完全值得，将来有机会的话，可以在这里申请硕士学位，前提是钱和时间都要有',
         'https://www.coursera.org/'
       ],
-      ['csdiy', 'https://csdiy.wiki/images/favicon.ico', '计算机自学网站，都是硬核的东西，作者是北大的博士生，学习一点就够了', 'https://csdiy.wiki/']
+      ['csdiy', 'https://csdiy.wiki/images/favicon.ico', '计算机自学网站，都是硬核的东西，作者是北大的博士生，学习一点就够了', 'https://csdiy.wiki/'],
+      [
+        'leanhub',
+        'https://leanpub.com/assets/favicons/favicon-32x32-9a0898109481d6450269c966cdf6a2d7.png',
+        '书店。从isocpp可以看到许多优秀的作者在那里卖书，需要用美元支付，有机会办个境外支付的卡',
+        'https://leanpub.com/'
+      ],
+      ['Udemy', 'https://www.udemy.com/staticx/udemy/images/v8/favicon-32x32.png', '优秀的课程网站，比Coursela资源多', 'https://www.udemy.com/'],
+      [
+        'z-library',
+        `<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="48" height="48" rx="4" fill="white"/>
+<path d="M16.5635 5.5177L23.1434 25.4927L23.1463 25.4916L23.2292 25.7334C23.5446 25.6031 23.8863 25.5243 24.2479 25.519C24.6351 25.5126 25.009 25.5891 25.3542 25.7299L25.4336 25.5097L25.4465 25.5143L31.7921 5.5177H31.4025L25.186 25.0862L29.0603 5.5177H28.6824L24.8886 24.6259L26.7052 5.5177H26.332L24.478 25.1668C24.4511 25.165 24.4255 25.161 24.3986 25.1598L24.4383 25.1592L24.5551 5.5177H24.1819L24.0656 25.165H24.075C24.0586 25.1662 24.0429 25.1691 24.0265 25.1703L22.401 5.5177H22.0266L23.613 24.8087L19.4519 5.5177H19.0716L23.1246 24.3082L16.9554 5.5177H16.5635V5.5177Z" fill="#C7C6C7"/>
+<path d="M26.2748 44.2895V32.4615H22.0815V44.2895L24.1779 42.1318L26.2748 44.2895Z" fill="#49AFD0"/>
+<path d="M37.7385 4.2002L32.7492 5.51793L25.2961 25.7108H23.2424L15.8202 5.41044L10.7993 4.2002L20.4206 30.3462L28.2148 30.3538L37.7385 4.2002Z" fill="#626062"/>
+</svg>`,
+        '可以下载很多盗版书，有机会还是买正版书吧...',
+        'https://z-library-zlibgs.github.io/'
+      ]
     ]
   }
 ];
